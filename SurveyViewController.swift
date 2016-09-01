eb102b82716736c2cb254041166d73ef7fb7619d
Justin Carver <15Jcarver202@gmail.com>
2016-09-01 10:37:52 -0600

    Initial Commit

---
+++ SurveyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SurveyViewController.swift
+//  Survey
+//
+//  Created by Justin Carver on 9/1/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
